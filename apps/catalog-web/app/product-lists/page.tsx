e6c73c8a08f2ece8ef1599f42a539807dd608178
Wes Cole <wcole1@godaddy.com>
2024-01-28 21:34:07 -0500

    .

--- apps/catalog-web/app/product-lists/page.tsx
+++ apps/catalog-web/app/product-lists/page.tsx
@@ -1,26 +1,28 @@
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 
-// async function getData() {
-//   const res = await fetch(
-//     `${process.env.API_URL}/v2/commerce/stores/1234/product-lists?include=product-list-items`,
-//   );
+async function getData() {
+  const res = await fetch(
+    `${process.env.API_URL}/v2/commerce/stores/1234/product-lists?include=product-list-items`,
+  );
 
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-//   return res.json();
-// }
+  return res.json();
+}
 
 export default async function ProductListsPage() {
-  // const data = await getData();
+  const data = await getData();
+
+  console.log(data);
 
   return (
     <div className="space-y-8">
       <h2 className="text-2xl font-bold">Product Lists</h2>
       <div>
-        <DataTable columns={columns} data={[]} />
+        <DataTable columns={columns} data={data?.productLists} />
       </div>
     </div>
   );
